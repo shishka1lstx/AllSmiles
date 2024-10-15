3c2fcf1da1cbb8eb21ff47b6894f25a9cbea6ca3
Narek <benz.hokobyan@gmail.com>
2024-10-15 23:48:52 +0300

    Update mainscript.js

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,21 +1,3 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    const block = document.querySelector('.red');
-    const block2 = document.querySelector('.green');
-    const block3 = document.querySelector('.blue');
-    const btn = document.querySelector('.btn');
     
-  
-    window.addEventListener('scroll', () => {
-        btn.click();
-        let scrollTop = window.scrollY;
-        if (scrollTop <= block.offsetHeight){
-            block.style.transform = `translateY(${scrollTop * 0.3}px)`;
-        }
-       
-        if (scrollTop - 5 - block.offsetHeight < block2.offsetHeight  && scrollTop > block.offsetHeight){
-            console.log('hi');
-            block2.style.transform = `translateY(${(scrollTop - block.offsetHeight ) * 0.3}px)`
-        }
-        
-    });
 });
